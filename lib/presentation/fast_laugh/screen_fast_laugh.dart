43475bc4c830193e3238e634f836819f32ce5e5d
Muhammed sinan <sinanzzsinu70@gmail.com>
2024-11-22 19:01:48 +0530

    Screen Fast Laugh Ui completed

--- lib/presentation/fast_laugh/screen_fast_laugh.dart
+++ lib/presentation/fast_laugh/screen_fast_laugh.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:netflix/presentation/fast_laugh/widget/video_list_item.dart';
 
 class ScreenFastLaugh extends StatelessWidget {
   const ScreenFastLaugh({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Fast Laugh"),),);
+    return Scaffold(
+      body: SafeArea(child: PageView(
+        scrollDirection: Axis.vertical,
+        children: List.generate(10, (index)=>VideoListItem(
+          index: index
+        )),
+      )),
+    );
   }
 }
---
+++ lib/presentation/fast_laugh/widget/video_list_item.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:netflix/core/colors/colors.dart';
+import 'package:netflix/presentation/search/widget/search_idl.dart';
+
+class VideoListItem extends StatelessWidget {
+  final int index;
+  VideoListItem({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          color: Colors.accents[index % Colors.accents.length],
+        ),
+        Align(
+          alignment: Alignment.bottomLeft,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                //left Side
+                CircleAvatar(
+                  backgroundColor: Colors.black.withOpacity(0.5),
+                  radius: 25,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.volume_mute,
+                      size: 35,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                //right Side
+                const Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                     Padding(
+                      padding:  EdgeInsets.symmetric(vertical: 10),
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(imageUrl),
+                        radius: 26,
+                      ),
+                    ),
+                    VideoActionWidget(icon: Icons.emoji_emotions, title: "LOL"),
+                    VideoActionWidget(icon: Icons.add, title: "My List"),
+                    VideoActionWidget(icon: Icons.share, title: "Share"),
+                    VideoActionWidget(icon: Icons.play_arrow, title: "Play")
+                  ],
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class VideoActionWidget extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  const VideoActionWidget({
+    super.key,
+    required this.icon,
+    required this.title
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 6),
+      child: Column(
+        children: [
+          Icon(icon,size: 30,color: Colors.white,),
+          Text(title,style: const TextStyle(
+            color: kWhiteColor,
+            fontSize: 16
+          ),)
+        ],
+      ),
+    );
+  }
+}
